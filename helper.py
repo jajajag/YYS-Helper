3521acfcf471acbde7dea094eadbabde6ba935c0
jajajag <zzzsgsb@gmail.com>
2019-06-05 20:26:19 -0400

    Update helper.py
    Update better sleeping time, reject collaboration and add single battle(御灵，御魂) to the script file.

--- helper.py
+++ helper.py
@@ -1,29 +1,60 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import win32gui, win32con, win32api, win32ui
-import time, random
+import time, random, sys
 from ctypes import windll
 from PIL import Image
 
-# Generate random point of next click
-def rand_next(width, height):
-    while True:
-        x = random.randint(1, width - 2)
-        y = random.randint(1, height - 2)
-        if x < 80 and y < 90:
-            continue
-        elif x > 550 and y < 35:
-            continue
-        elif x > 160 and x < 710 and y > 100 and y < 600:
-            continue
-        elif x > 90 and y > 427 and y < 457:
-            continue
-        return x, y
+# Generate random point of next click and sleeping time
+def rand_point(screen):
+    # Assign properties
+    height, width, _ = screen.shape
+    # If we should reject the collaboraiton
+    if (screen[123][473] == [179, 146, 118]).all():
+        x = random.randint(512, 534)
+        y = random.randint(74, 92)
+        # If reject, click the button suddenly
+        sleep_time = 0.5
+    # If we should end this battle
+    elif (screen[468][148] == [255, 255, 255]).all():
+        while True:
+            x = random.randint(1, width - 2)
+            y = random.randint(1, height - 2)
+            # Stat area
+            if x < 80 and y < 90:
+                continue
+            # Dialog area
+            elif x > 550 and y < 35:
+                continue
+            # Equip area
+            elif x > 160 and x < 710 and y > 100 and y < 600:
+                continue
+            # Buff area
+            elif x > 90 and y > 424 and y < 457:
+                continue
+            break
+        sleep_time = 1.8 + random.random() * 1.5
+    # If we are in party
+    elif (screen[400][487] == [87, 68, 46]).all():
+        x = random.randint(640, 752)
+        y = random.randint(384, 418)
+        # If we have just start the battle, don't need to check for 10 seconds
+        sleep_time = 10
+    # If we want to start a single battle
+    elif (screen[83][498] == [57, 42, 30]).all():
+        x = random.randint(597, 675)
+        y = random.randint(316, 347)
+        # If we have just start the battle, don't need to check for 10 seconds
+        sleep_time = 10
+    # If in other cases
+    else:
+        x, y = None, None
+        sleep_time = 1.5
+
+    return x, y, sleep_time
 
 # Generate random point of starting button
 def rand_start(width, height):
-    x = random.randint(640, 752)
-    y = random.randint(384, 418)
     return x, y
 
 def main():
@@ -69,33 +100,29 @@ def main():
         win32gui.ReleaseDC(hwnd, hwndDC)
         
         # Search white pixels
-        #for i in range(470):
-        #    for j in range(850):
-        #        #if screen[i][j][0] == 255 and screen[i][j][1] == 255 and screen[i][j][2] == 255:
-        #        #if screen[i][j][0] == 0 and screen[i][j][1] == 0 and screen[i][j][2] == 0:
+        #for i in range(10, 470):
+        #    for j in range(10, 850):
+        #        if screen[i][j][0] == 255 and screen[i][j][1] == 255 and screen[i][j][2] == 255:
+        #        if screen[i][j][0] == 0 and screen[i][j][1] == 0 and screen[i][j][2] == 0:
         #        if screen[i][j][0] == screen[400][487][0] \
         #                and screen[i][j][1] == screen[400][487][1] \
         #                and screen[i][j][2] == screen[400][487][2]:
         #            print(i, j)
+        #break
 
-        # Click if the pixel is white
-        if screen[468][148][0] == 255 and screen[468][148][1] == 255 and screen[468][148][2] == 255:
-            x, y = rand_next(width, height)
-        # Click the start button if in party
-        elif screen[400][487][0] == 87 and screen[400][487][1] == 68 and screen[400][487][2] == 46:
-            x, y = rand_start(width, height)
-        else:
-            # Sleep for random time
-            time.sleep(1.5 + random.random() * 0.8)
-            continue
-        l_param = win32api.MAKELONG(x, y)
-        win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0, l_param)
-        #win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, l_param)
-        win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, 0, l_param)
-        time.sleep(0.03 + random.random() * 0.02)
-        win32api.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, l_param)
+        # Generate random point and sleep_time from the screenshot
+        x, y, sleep_time = rand_point(screen)
+        if x != None:
+            l_param = win32api.MAKELONG(x, y)
+            win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0, l_param)
+            #win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, l_param)
+            win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, 0, l_param)
+            time.sleep(0.03 + random.random() * 0.02)
+            win32api.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, l_param)
+            print(".", end="")
+            sys.stdout.flush()
         # Sleep for random time
-        time.sleep(1.5 + random.random() * 0.8)
+        time.sleep(sleep_time)
 
 if __name__ == '__main__':
     main()
