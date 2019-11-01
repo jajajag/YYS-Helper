24a47830c32d0b3119d387d651cc3b5acb92344a
Jag Zhang <zzzsgsb@gmail.com>
2019-11-01 10:44:12 -0400

    Update multi configuration files.

--- yys_helper.py
+++ yys_helper.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 class YYS_Helper(object):
     def __init__(self,
             config_file='config.txt',
+            num_runs=100,
             class_name="Win32Window0",
             title_name="阴阳师-网易游戏"):
         # Find the hwnd of the window
@@ -31,11 +32,18 @@ class YYS_Helper(object):
         self.saveDC.SelectObject(self.saveBitMap)
 
         # Initialize configuration for target pixel and clicking area
+        print("配置文件config_xxx.txt(默认%s，输入xxx)：" % config_file, end="")
+        config_file_input = input()
+        config_file = "config_" + config_file_input + ".txt" \
+                if config_file_input != "" else config_file
         self.configs = self.read_file(config_file)
+
         # Get the input for total running time
-        print("运行次数：", end="")
+        print("运行次数(默认%d)：" % num_runs, end="")
+        num_runs_input = input()
+        num_runs = int(num_runs_input) if num_runs_input != "" else num_runs
         # Initialize progressing bar with the total running times
-        self.pbar = tqdm(total=int(input()), ascii=True)
+        self.pbar = tqdm(total=num_runs, ascii=True)
         self.pbar_time = 0
 
     def __del__(self):
